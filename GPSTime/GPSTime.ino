5f28f350a8bd54e2adb72f8a255ec70f1511756d
Mitchel Rojas Rivera <mrojasr@usm.cl>
2023-11-03 16:13:29 -0300

    Se agrego la caracteristica de ajustar el RTC a travez de GPS

--- GPSTime/GPSTime.ino
+++ GPSTime/GPSTime.ino
@@ -1,12 +1,17 @@
 #include <TinyGPSPlus.h>
 #include <SoftwareSerial.h>
 #include <HardwareSerial.h>
+#include <RTClib.h>
 #include "GPS.h"
+#include "RTC.h"
 
 void setup() {
   initGPS();
+  delay(10000);
+  initRTC();
 }
 
 void loop() {
   configGPS();
+  configRTC();
 }
---
+++ GPSTime/RTC.h
@@ -0,0 +1,41 @@
+RTC_DS3231 rtc;
+
+void initRTC()
+{
+  Serial.begin(9600);
+  if (!rtc.begin()) {
+    Serial.println("No se pudo encontrar el módulo RTC DS3231. Verifica la conexión.");
+    while (true);
+  }
+}
+
+void configRTC()
+{
+  TinyGPSDate d = gps.date;
+  TinyGPSTime t = gps.time;
+
+  rtc.adjust(DateTime(d.year(), d.month(), d.day(), t.hour() - 3, t.minute(), t.second()));
+
+  DateTime now = rtc.now(); 
+  Serial.print(F("RTC Date/Time: "));
+  Serial.print(now.year());
+  Serial.print(F("/"));
+  if (now.month() < 10) Serial.print(F("0"));
+  Serial.print(now.month());
+  Serial.print(F("/"));
+  if (now.day() < 10) Serial.print(F("0"));
+  Serial.print(now.day());
+
+  Serial.print(F(" "));
+
+  if (now.hour() < 10) Serial.print(F("0"));
+  Serial.print(now.hour());
+  Serial.print(F(":"));
+  if (now.minute() < 10) Serial.print(F("0"));
+  Serial.print(now.minute());
+  Serial.print(F(":"));
+  if (now.second() < 10) Serial.print(F("0"));
+  Serial.print(now.second());
+
+  Serial.println();
+}
